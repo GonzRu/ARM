482126ce0f4d5a8ebcf0fa4397038134b247d0da
Smirnov Denis <gonzru@gmail.com>
2014-01-21 13:56:56 +0400

    Рефакторинг + метод по показу тегов группы устройства

--- 01_HMIWinFormsClient_158/HelperControlsLibrary/BlockViewControl.Designer.cs
+++ 01_HMIWinFormsClient_158/HelperControlsLibrary/BlockViewControl.Designer.cs
@@ -31,8 +31,8 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupsTreeView = new System.Windows.Forms.TreeView();
+            this.tagsTableDataGridView = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -41,7 +41,7 @@
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tagsTableDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -65,46 +65,46 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupsTreeView);
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel2.Controls.Add(this.tagsTableDataGridView);
             this.splitContainer1.Size = new System.Drawing.Size(880, 500);
             this.splitContainer1.SplitterDistance = 443;
             this.splitContainer1.TabIndex = 0;
             // 
-            // treeView1
+            // groupsTreeView
             // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.HideSelection = false;
-            this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(443, 500);
-            this.treeView1.TabIndex = 0;
-            this.treeView1.Click += new System.EventHandler(this.TreeView1Click);
+            this.groupsTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupsTreeView.HideSelection = false;
+            this.groupsTreeView.Location = new System.Drawing.Point(0, 0);
+            this.groupsTreeView.Name = "groupsTreeView";
+            this.groupsTreeView.Size = new System.Drawing.Size(443, 500);
+            this.groupsTreeView.TabIndex = 0;
+            this.groupsTreeView.Click += new System.EventHandler(this.TreeView1Click);
             // 
-            // dataGridView1
+            // tagsTableDataGridView
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
+            this.tagsTableDataGridView.AllowUserToAddRows = false;
+            this.tagsTableDataGridView.AllowUserToDeleteRows = false;
+            this.tagsTableDataGridView.AllowUserToResizeRows = false;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tagsTableDataGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.tagsTableDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tagsTableDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tagsTableDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(433, 500);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridRowCellEndEdit);
+            this.tagsTableDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tagsTableDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.tagsTableDataGridView.Name = "tagsTableDataGridView";
+            this.tagsTableDataGridView.RowHeadersVisible = false;
+            this.tagsTableDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tagsTableDataGridView.Size = new System.Drawing.Size(433, 500);
+            this.tagsTableDataGridView.TabIndex = 0;
+            this.tagsTableDataGridView.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridRowCellEndEdit);
             // 
             // Column1
             // 
@@ -136,7 +136,7 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tagsTableDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -145,8 +145,8 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TreeView groupsTreeView;
+        private System.Windows.Forms.DataGridView tagsTableDataGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
