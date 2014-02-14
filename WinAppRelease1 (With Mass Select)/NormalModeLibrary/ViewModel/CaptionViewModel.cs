02ebffaf6823f2883dd1c028a8f557a6e4e5d497
Smirnov Denis <gonzru@gmail.com>
2014-02-14 09:28:02 +0400

    Добавил CaptionViewModel - для отображения заголовка в панели

---
+++ WinAppRelease1 (With Mass Select)/NormalModeLibrary/ViewModel/CaptionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NormalModeLibrary.ViewModel
+{
+    public class CaptionViewModel : ViewModelBase
+    {
+        private string _captionText;
+
+        public string CaptionText
+        {
+            get { return _captionText; }
+            set { _captionText = value; OnPropertyChanged("caption"); }
+        }
+
+        public CaptionViewModel(string caption)
+        {
+            _captionText = caption;
+        }
+
+        internal override ViewModelBase Copy()
+        {
+            return new CaptionViewModel(_captionText);
+        }
+    }
+}
