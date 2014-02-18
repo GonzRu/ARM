828e88b7cdda80349c1f1ab38ee82194841c2628
Smirnov Denis <gonzru@gmail.com>
2014-02-18 11:53:04 +0400

    Добавил ViewElementHost

---
+++ WinAppRelease1 (With Mass Select)/NormalModeLibrary/Windows/NormalModePanels/ViewElementHost.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using NormalModeLibrary.ViewModel;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+
+namespace NormalModeLibrary.Windows
+{
+    public class ViewElementHost : ElementHost, INormalModePanel
+    {
+        #region private
+        private bool _canMove;
+        private Point _mouseDownLocation;
+        #endregion
+
+        #region Properties
+        public Places Place { get; set; }
+
+        public PanelViewModel Component
+        {
+            get
+            {
+                TableControl tc = (TableControl)Child;
+                return (PanelViewModel)tc.DataContext;
+            }
+            set
+            {
+                TableControl tc = (TableControl)Child;
+                tc.DataContext = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public ViewElementHost()
+        {
+            TableControl tc = new TableControl(this);
+            Child = tc;
+            //tc.MouseDown += TcOnMouseDown;
+            //tc.MouseUp += TcOnMouseUp;
+            //tc.MouseMove += TcOnMouseMove;
+        }
+
+        #endregion
+
+        #region Public metods
+        public void ActivatedComponent()
+        {
+            if (Component != null)
+            {
+                this.Text = Component.Caption;
+
+                this.Top = Component.Top;
+                this.Left = Component.Left;
+                this.Width = Component.Width;
+                this.Height = Component.Height;
+
+                if (Component.IsAutomaticaly)
+                    Visible = false;
+            }
+        }
+
+        public void DeactivatedComponent()
+        {
+            
+        }
+
+        public void SetOnEditMode()
+        {
+        }
+
+        public void SetOffEditMode()
+        {
+        }
+
+        public void UpdateWorkMode()
+        {
+            if (Component.IsVisible)
+            {
+                if (Component.IsAutomaticaly)
+                    Hide();
+            }
+            else
+                Hide();
+        }
+
+        public void ShowIfNeed()
+        {
+            if (Component.IsVisible)
+                if (!Component.IsAutomaticaly)
+                {
+                    Visible = true;
+                }
+        }
+        #endregion
+    }
+}
