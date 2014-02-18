b2f1ef19e3c2e068fa721ea7d96a376e1c2101e4
Smirnov Denis <gonzru@gmail.com>
2014-02-18 09:47:49 +0400

    Add normal mode panel interface

---
+++ WinAppRelease1 (With Mass Select)/NormalModeLibrary/Windows/NormalModePanels/INormalModePanel.cs
@@ -0,0 +1,44 @@
+﻿using NormalModeLibrary.ViewModel;
+
+namespace NormalModeLibrary.Windows
+{
+    interface INormalModePanel
+    {
+        /// <summary>
+        /// Месторасположение панели
+        /// </summary>
+        Places Place { get; set; }
+
+        /// <summary>
+        /// Panel's data source
+        /// </summary>
+        PanelViewModel Component { get; set; }
+
+        /// <summary>
+        /// Activate panel
+        /// </summary>
+        void ActivatedComponent();
+
+        /// <summary>
+        /// Deactivate panel
+        /// </summary>
+        void DeactivatedComponent();
+
+        /// <summary>
+        /// Set on in Edit Mode
+        /// </summary>
+        void SetOnEditMode();
+
+        /// <summary>
+        /// Set off edit mode
+        /// </summary>
+        void SetOffEditMode();
+
+        /// <summary>
+        /// Update work mode (Always visible, Automaticaly, Never visible)
+        /// </summary>
+        void UpdateWorkMode();
+
+        void ShowIfNeed();
+    }
+}
