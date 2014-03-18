138c7af5d214f65ff204180982a72efe0c661146
Smirnov Denis <gonzru@gmail.com>
2014-03-18 14:29:14 +0400

    add IMessageProvider

--- 01_HMIWinFormsClient_158/MessagePanel/IMessageProvider.cs
+++ 01_HMIWinFormsClient_158/MessagePanel/IMessageProvider.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using MessagePanel.MessagePanelService;
 
 namespace MessagePanel
 {
     public interface IMessageProvider
     {
+        #region Events
+        /// <summary>
+        /// Событие обновления списка сообщений
+        /// </summary>
+        event Action MessagesUpdated;
+
+        /// <summary>
+        /// События появления сообщений, на которые пользователь должен обратить внимание
+        /// </summary>
+        event Action AlarmMessagesAppeared;
+
+        #endregion
+
+        #region Metods
+        /// <summary>
+        /// Запускает работу объекта.
+        /// </summary>
+        void StartWork(int userId);
+
+        /// <summary>
+        /// Получить список сообщений
+        /// </summary>
+        List<TableEventLogAlarm> GetMessages();
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Получить или задать максимальное количество запрашиваемых сообщений
+        /// </summary>
+        int MessageCount { get; set; }
+        #endregion
     }
 }
