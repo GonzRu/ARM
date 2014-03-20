ef11deea021416361b3a448515d05a6074d5a692
Smirnov Denis <gonzru@gmail.com>
2014-03-20 11:58:17 +0400

    add empty KvitWindow

---
+++ 01_HMIWinFormsClient_158/HMI_MT/KvitWindow.cs
@@ -0,0 +1,12 @@
+
+namespace HMI_MT
+{
+    /// <summary>
+    /// Окно, выполняющее квитирование в фоне и отображающее прогресс операции с возможностью отмены
+    /// </summary>
+    public partial class KvitWindow : Form
+    {
+        #region Constructors
+        #endregion Constructors
+    }
+}
