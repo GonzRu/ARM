f1f41e3843ae45b05fe0068fdb4207384e134b1b
Smirnov Denis <gonzru@gmail.com>
2014-03-21 08:08:16 +0400

    fix  kvitWindow ui

--- 01_HMIWinFormsClient_158/HMI_MT/KvitWindow.Designer.cs
+++ 01_HMIWinFormsClient_158/HMI_MT/KvitWindow.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.mainLabel = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.CancelButton = new System.Windows.Forms.Button();
             this.OkButton = new System.Windows.Forms.Button();
-            this.mainLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.YesButton = new System.Windows.Forms.Button();
             this.NoButton = new System.Windows.Forms.Button();
+            this.YesButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -57,39 +57,9 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 0F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 0F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(336, 157);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(347, 173);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.progressBar1.Location = new System.Drawing.Point(3, 80);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(330, 34);
-            this.progressBar1.TabIndex = 1;
-            // 
-            // CancelButton
-            // 
-            this.CancelButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CancelButton.Location = new System.Drawing.Point(3, 120);
-            this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(330, 1);
-            this.CancelButton.TabIndex = 2;
-            this.CancelButton.Text = "Отмена";
-            this.CancelButton.UseVisualStyleBackColor = true;
-            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
-            // 
-            // OkButton
-            // 
-            this.OkButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.OkButton.Location = new System.Drawing.Point(3, 120);
-            this.OkButton.Name = "OkButton";
-            this.OkButton.Size = new System.Drawing.Size(330, 1);
-            this.OkButton.TabIndex = 3;
-            this.OkButton.Text = "Ок";
-            this.OkButton.UseVisualStyleBackColor = true;
-            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
             // 
             // mainLabel
             // 
@@ -98,30 +68,50 @@
             this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.mainLabel.Location = new System.Drawing.Point(3, 0);
             this.mainLabel.Name = "mainLabel";
-            this.mainLabel.Size = new System.Drawing.Size(330, 77);
+            this.mainLabel.Size = new System.Drawing.Size(341, 91);
             this.mainLabel.TabIndex = 6;
             this.mainLabel.Text = "Точно квитировать?";
             this.mainLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(3, 94);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(341, 34);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CancelButton.Location = new System.Drawing.Point(3, 134);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(341, 1);
+            this.CancelButton.TabIndex = 2;
+            this.CancelButton.Text = "Отмена";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // OkButton
+            // 
+            this.OkButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OkButton.Location = new System.Drawing.Point(3, 134);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(341, 1);
+            this.OkButton.TabIndex = 3;
+            this.OkButton.Text = "Ок";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.NoButton);
             this.panel1.Controls.Add(this.YesButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 120);
+            this.panel1.Location = new System.Drawing.Point(3, 134);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(330, 34);
+            this.panel1.Size = new System.Drawing.Size(341, 36);
             this.panel1.TabIndex = 7;
-            // 
-            // YesButton
-            // 
-            this.YesButton.Location = new System.Drawing.Point(53, 0);
-            this.YesButton.Name = "YesButton";
-            this.YesButton.Size = new System.Drawing.Size(100, 30);
-            this.YesButton.TabIndex = 0;
-            this.YesButton.Text = "Да";
-            this.YesButton.UseVisualStyleBackColor = true;
-            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
             // 
             // NoButton
             // 
@@ -133,13 +123,25 @@
             this.NoButton.UseVisualStyleBackColor = true;
             this.NoButton.Click += new System.EventHandler(this.NoButton_Click);
             // 
+            // YesButton
+            // 
+            this.YesButton.Location = new System.Drawing.Point(53, 0);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(100, 30);
+            this.YesButton.TabIndex = 0;
+            this.YesButton.Text = "Да";
+            this.YesButton.UseVisualStyleBackColor = true;
+            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
+            // 
             // KvitWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 157);
+            this.ClientSize = new System.Drawing.Size(347, 173);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "KvitWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "KvitWindow";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
