fb868c83e00454a09c331f0151b777943fb2f767
Smirnov Denis <gonzru@gmail.com>
2014-04-03 17:25:03 +0400

    Рефакторинг

--- 01_HMIWinFormsClient_158/DevicesLibrary/DeviceFormFactory.cs
+++ 01_HMIWinFormsClient_158/DevicesLibrary/DeviceFormFactory.cs
@@ -15,47 +15,41 @@ namespace DevicesLibrary
     {
         public static IDeviceForm CreateForm(Form parent, uint dsGuid, uint devGuid, ArrayList arrFrm)
         {
-            // извлекаем описание из PrgDevCFG.cdp
             try
             {
-                var xeDescDev = HMI_Settings.CONFIGURATION.GetDeviceXMLDescription((int)dsGuid, "MOA_ECU", (int)devGuid);
-
-                if (xeDescDev == null)
+                foreach (IDeviceForm f in arrFrm)
                 {
-                    MessageBox.Show(string.Format("Элемент не привязан к устройству (GUID = {0}) в текущей конфигурации.", devGuid), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (f.Guid == devGuid)
+                    {
+                        ((Form) f).Focus();
+                        return f;
+                    }
+                }
+
+                var device = HMI_Settings.CONFIGURATION.GetLink2Device(dsGuid, devGuid);
+
+                if (device == null)
+                {
+                    //MessageBox.Show(string.Format("Элемент не привязан к устройству (GUID = {0}) в текущей конфигурации.", devGuid), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     throw new Exception( string.Format( "{0} : DeviceFormFactory : {1}", DateTime.Now, "Элемент не привязан к устройству в текущей конфигурации" ) );
                 }
 
-                var objGuid = xeDescDev.Attribute("objectGUID").Value;
-
-                xeDescDev = xeDescDev.Element( "DescDev" );
-
-                var strTypeBlock = xeDescDev.Element( "nameELowLevel" ).Value;
-
-                string FrmTagsDescript = Path.GetDirectoryName(HMI_Settings.PathToConfigurationFile) +
-                    "\\Configuration\\0#DataServer\\Devices\\" + objGuid + '@' + strTypeBlock + ".cfg";
-
-                if (!File.Exists(FrmTagsDescript))
+                string pathToDeviceFile = Path.GetDirectoryName(HMI_Settings.PathToConfigurationFile) + "\\Configuration\\0#DataServer\\Devices\\" + device.UniObjectGUID + '@' + device.TypeName + ".cfg";
+                if (!File.Exists(pathToDeviceFile))
                 {
                     //MessageBox.Show(string.Format("Файл описания формы не существует ({0})", FrmTagsDescript), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     throw new Exception(
                         string.Format( "{0} : DeviceFormFactory : {1}", DateTime.Now, "Элемент не привязан к устройству в текущей конфигурации" ) );
                 }
 
-                //var folderDevDescript = string.Format("{0}\\Project\\{1}\\", AppDomain.CurrentDomain.BaseDirectory, xeDescDev.Element( "nameR" ).Value);
-
                 DebugStatistics.WindowStatistics.AddStatistic( "Создание и инициализация формы." );
 
-                IDeviceForm deviceform = new FormBlock(strTypeBlock, dsGuid, devGuid);
+                IDeviceForm deviceform = new FormBlock(device.TypeName, dsGuid, devGuid);
                 
                 DebugStatistics.WindowStatistics.AddStatistic( "Создание и инициализация формы завершено." );
 
                 if (HMI_Settings.IsDebugMode)
-                {
-                    var device = HMI_Settings.CONFIGURATION.GetLink2Device(dsGuid, devGuid);
-
                     (deviceform as Form).Text = String.Format("({0}) {1} ({2})", devGuid, device.Description, device.TypeName);
-                }
                 else
                     (deviceform as Form).Text = CommonUtils.CommonUtils.GetDispCaptionForDevice((int)devGuid);
 
@@ -70,15 +64,7 @@ namespace DevicesLibrary
                         MessageBox.Show( "Терминал недоступен или с ним нет связи", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning );
                 }
 
-                foreach ( IDeviceForm f in arrFrm )
-                {
-                    if ( f.Guid == deviceform.Guid )
-                    {
-                        ( (Form)f ).Focus( );
-                        ( (Form)deviceform ).Dispose( );
-                        return f;
-                    }
-                }
+
 
                 DebugStatistics.WindowStatistics.AddStatistic( "Запуск формы." );
 
