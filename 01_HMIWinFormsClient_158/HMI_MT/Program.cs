e59382b1299415e2c16a0a79d6bba220c5f8717d
Smirnov Denis <gonzru@gmail.com>
2014-04-03 17:08:52 +0400

    Из program.cs  удалена иницилизация TracerouteLib и добавлен запуск SplashScreen

--- 01_HMIWinFormsClient_158/HMI_MT/Program.cs
+++ 01_HMIWinFormsClient_158/HMI_MT/Program.cs
@@ -1,25 +1,20 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
 using System.Diagnostics;
 using System.Threading;
-using System.IO;
-using System.Linq;
-using System.Xml.Linq;
-using HMI_MT_Settings;
+using System.Windows.Forms;
+using HMI_MT.Windows.SplashScreen;
+using TraceSourceLib;
 
 namespace HMI_MT
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         //[MTAThread] // !!!!!!!!!!!!!!!!!!!!!!!!!!!! === для OPC это значение обязательно === !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        static void Main()
+        private static void Main()
         {
             // обнаруживаем предыдущий экземпляр приложения
             //foreach(Process p in Process.GetProcessesByName(Application.ProductName))
@@ -34,98 +29,66 @@ namespace HMI_MT
             // регистрация глобального обработчика ошибок
             // в отладчке глобальный обработчик ошибок не работает !!!
 
-            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler( Application_ThreadException );
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler( CurrentDomain_UnhandledException );
+            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault( false );
+            Application.SetCompatibleTextRenderingDefault(false);
 
-            // проверяем существование файла конфигурации  проекта Project.cfg и файла конфигурации устройств проекта
-            string PathToPrjFile = AppDomain.CurrentDomain.BaseDirectory + "Project" + Path.DirectorySeparatorChar + "Project.cfg";
+            Application.Run(new SplashScreenWindow());
 
-            if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "Project"))
-            {
-                MessageBox.Show("Не найдена папка проекта.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            if ( !File.Exists( PathToPrjFile ) )
-            {
-                MessageBox.Show("Не найден файл Project.cfg", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            try
-            {
-                HMI_Settings.PathToPrjFile = PathToPrjFile;
-                HMI_Settings.XDoc4PathToPrjFile = XDocument.Load( HMI_Settings.PathToPrjFile );
-            }
-            catch ( Exception )
-            {
-                throw new Exception( string.Format( "(423) : MainForm.cs : SetNamesCfgPrgFiles() : Несуществующий файл = {0}", PathToPrjFile ) );
-            }
-
-            if ( HMI_MT_Settings.HMI_Settings.XDoc4PathToPrjFile.Element( "Project" ).Elements( "TextListener" ).Count() > 0 )
-            {
-                string namef = HMI_MT_Settings.HMI_Settings.XDoc4PathToPrjFile.Element( "Project" ).Element( "TextListener" ).Attribute( "fileName" ).Value;
-                int sizef = int.Parse( HMI_MT_Settings.HMI_Settings.XDoc4PathToPrjFile.Element( "Project" ).Element( "TextListener" ).Attribute( "fsize_kbait" ).Value );
-
-                CommonUtils.LogMonitoring lm;
-
-                if ( File.Exists( namef ) )
-                    lm = new CommonUtils.LogMonitoring( string.Format( "{0}\\{1}", AppDomain.CurrentDomain.BaseDirectory, namef ), sizef );
-            }
-
-            StartTrace();
-
-            TraceSourceLib.TraceSourceDiagMes.WriteDiagnosticMSG( TraceEventType.Critical, 37, DateTime.Now.ToString() + " : (37) Program.cs : Main() : Запуск приложения" );
-            TraceSourceLib.TraceSourceDiagMes.FlushLog();
-
-            Application.Run( new MainForm() );
-        }
-        /// <summary>
-        ///старт процесса трассировки приложения
-        /// </summary>
-        private static void StartTrace()
-        {
-            TraceSourceLib.TraceSourceDiagMes.CreateLog( "HMI_MT_Client_TraceSource" );
-            TraceSourceLib.TraceSourceDiagMes.tracesource.TraceData( TraceEventType.Verbose, 1, "StartTest" );
-            TraceSourceLib.TraceSourceDiagMes.tracesource.Flush();
+            Application.Run(new MainForm());
         }
 
-        static void CurrentDomain_UnhandledException( object sender, UnhandledExceptionEventArgs e )
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             // подготовка содержательного сообщения
-            string msg = String.Format( "(Program.cs) В приложении обнаружена ошибка: \n\n {0}", e.GetType().ToString() );
+            string msg = String.Format("(Program.cs) В приложении обнаружена ошибка: \n\n {0}", e.GetType());
             // желает ли пользователь завершить приложение
-            DialogResult rezult = MessageBox.Show( msg + "\n" + Process.GetCurrentProcess().WorkingSet64.ToString() + " (ФП); " + Process.GetCurrentProcess().VirtualMemorySize64.ToString() + " (ВП); "
-            + Process.GetCurrentProcess().Threads.Count.ToString() + " - потоков; " + Process.GetCurrentProcess().HandleCount.ToString() + " - дескр.; ", "Ошибка", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error );
-            if ( rezult == DialogResult.Abort )
+            DialogResult rezult =
+                MessageBox.Show(
+                    msg + "\n" + Process.GetCurrentProcess().WorkingSet64.ToString() + " (ФП); " +
+                    Process.GetCurrentProcess().VirtualMemorySize64.ToString() + " (ВП); "
+                    + Process.GetCurrentProcess().Threads.Count.ToString() + " - потоков; " +
+                    Process.GetCurrentProcess().HandleCount.ToString() + " - дескр.; ", "Ошибка",
+                    MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            if (rezult == DialogResult.Abort)
 
-                TraceSourceLib.TraceSourceDiagMes.WriteDiagnosticMSG( TraceEventType.Critical, 52, DateTime.Now.ToString() + " : (52) Program.cs : CurrentDomain_UnhandledException() : Выход из приложения" );
-            TraceSourceLib.TraceSourceDiagMes.FlushLog();
+                TraceSourceDiagMes.WriteDiagnosticMSG(TraceEventType.Critical, 52,
+                                                      DateTime.Now.ToString() +
+                                                      " : (52) Program.cs : CurrentDomain_UnhandledException() : Выход из приложения");
+            TraceSourceDiagMes.FlushLog();
 
             Application.Exit();
         }
+
         /// <summary>
         /// 
         /// </summary>
         /// <param Name="sender"></param>
         /// <param Name="e"></param>
-        static void Application_ThreadException( object sender, ThreadExceptionEventArgs e )
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
         {
             try
             {
                 // подготовка содержательного сообщения
-                string msg = String.Format( "(Program.cs) В приложении обнаружена ошибка: \n\n {0} \n\n {1}", e.Exception.Message, e.Exception.StackTrace );
+                string msg = String.Format("(Program.cs) В приложении обнаружена ошибка: \n\n {0} \n\n {1}",
+                                           e.Exception.Message, e.Exception.StackTrace);
 
                 // желает ли пользователь завершить приложение
-                switch ( MessageBox.Show( msg + "\n" + Process.GetCurrentProcess().WorkingSet64.ToString() + " (ФП); " + Process.GetCurrentProcess().VirtualMemorySize64.ToString() + " (ВП); "
-                    + Process.GetCurrentProcess().Threads.Count.ToString() + " - потоков; " + Process.GetCurrentProcess().HandleCount.ToString() + " - дескр.; ", "Ошибка", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error ).ToString() )
+                switch (
+                    MessageBox.Show(
+                        msg + "\n" + Process.GetCurrentProcess().WorkingSet64.ToString() + " (ФП); " +
+                        Process.GetCurrentProcess().VirtualMemorySize64.ToString() + " (ВП); "
+                        + Process.GetCurrentProcess().Threads.Count.ToString() + " - потоков; " +
+                        Process.GetCurrentProcess().HandleCount.ToString() + " - дескр.; ", "Ошибка",
+                        MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error).ToString())
                 {
                     case "Abort":
-                        TraceSourceLib.TraceSourceDiagMes.WriteDiagnosticMSG( TraceEventType.Critical, 74, DateTime.Now.ToString() + " : (74) Program.cs : Application_ThreadException() : Выход из приложения" );
-                        TraceSourceLib.TraceSourceDiagMes.FlushLog();
+                        TraceSourceDiagMes.WriteDiagnosticMSG(TraceEventType.Critical, 74,
+                                                              DateTime.Now.ToString() +
+                                                              " : (74) Program.cs : Application_ThreadException() : Выход из приложения");
+                        TraceSourceDiagMes.FlushLog();
 
                         Application.Exit();
                         break;
@@ -138,13 +101,20 @@ namespace HMI_MT
                 // если информационное окно отобразить не удалось, отобразим более простое сообщение
                 try
                 {
-                    MessageBox.Show( "(Program.cs) Приложение будет закрыто по ошибке\n" + Process.GetCurrentProcess().WorkingSet64.ToString() + " (ФП); " + Process.GetCurrentProcess().VirtualMemorySize64.ToString() + " (ВП); "
-                + Process.GetCurrentProcess().Threads.Count.ToString() + " - потоков; " + Process.GetCurrentProcess().HandleCount.ToString() + " - дескр.; ", "Критическая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    MessageBox.Show(
+                        "(Program.cs) Приложение будет закрыто по ошибке\n" +
+                        Process.GetCurrentProcess().WorkingSet64.ToString() + " (ФП); " +
+                        Process.GetCurrentProcess().VirtualMemorySize64.ToString() + " (ВП); "
+                        + Process.GetCurrentProcess().Threads.Count.ToString() + " - потоков; " +
+                        Process.GetCurrentProcess().HandleCount.ToString() + " - дескр.; ", "Критическая ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 finally
                 {
-                    TraceSourceLib.TraceSourceDiagMes.WriteDiagnosticMSG( TraceEventType.Critical, 93, DateTime.Now.ToString() + " : (93) Program.cs : Application_ThreadException() : Выход из приложения" );
-                    TraceSourceLib.TraceSourceDiagMes.FlushLog();
+                    TraceSourceDiagMes.WriteDiagnosticMSG(TraceEventType.Critical, 93,
+                                                          DateTime.Now.ToString() +
+                                                          " : (93) Program.cs : Application_ThreadException() : Выход из приложения");
+                    TraceSourceDiagMes.FlushLog();
 
                     Application.Exit();
                 }
